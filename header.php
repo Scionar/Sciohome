56b28e495e2b901587560c4c8fe8dd93cc4f6f63
scionar <scionarius@gmail.com>
2014-03-09 14:29:56 +0200

    Deleted unneeded function

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/logoFive.css" />
 		
 		<!-- Scripts -->
-		<?php wp_register_script('jquery'); ?>
 		<?php 
 			wp_enqueue_script(
 				'logoAnimation',
